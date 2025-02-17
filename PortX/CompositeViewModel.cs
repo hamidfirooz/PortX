ed8685f292c46d3eaeea7ed9655be2e9c86b9a4a
hamid firooz <hamidfiroozofficial@gmail.com>
2025-02-17 12:57:58 +0330

    Add Tabcontrol

---
+++ PortX/CompositeViewModel.cs
@@ -0,0 +1,21 @@
+﻿using PortX.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortX
+{
+    public class CompositeViewModel
+    {
+        public MenuViewModel MenuViewModel { get; set; }
+        public TabViewModel TabViewModel { get; set; }
+
+        public CompositeViewModel()
+        {
+            MenuViewModel = new MenuViewModel();
+            TabViewModel = new TabViewModel();
+        }
+    }
+}
---
+++ PortX/ViewModels/TabViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PortX.ViewModels
+{
+    public class TabViewModel : BaseViewModel
+    {
+        public ObservableCollection<TabItemModel> Tabs { get; set; }
+        public TabItemModel SelectedTab { get; set; }
+
+        public ICommand AddTabCommand { get; }
+        public ICommand RemoveTabCommand { get; }
+
+        public TabViewModel()
+        {
+            /*
+            Tabs = new ObservableCollection<TabItemModel>();
+            AddTabCommand = new RelayCommand(AddTab);
+            RemoveTabCommand = new RelayCommand(RemoveTab);
+
+            // Add a sample tab
+            Tabs.Add(new TabItemModel { Header = "Tab 1", Content = "Content of Tab 1" });
+            */
+            Tabs = new ObservableCollection<TabItemModel>
+            {
+                new TabItemModel { Header = "Tab 1", Content = "Content of Tab 1" },
+                new TabItemModel { Header = "Tab 2", Content = "Content of Tab 2" },
+                new TabItemModel { Header = "Tab 3", Content = "Content of Tab 3" },
+                new TabItemModel { Header = "Tab 4", Content = "Content of Tab 4" }
+            };
+
+            // انتخاب تب اول به طور پیش‌فرض
+            SelectedTab = Tabs[0];
+
+        }
+
+        private void AddTab()
+        {
+            Tabs.Add(new TabItemModel { Header = "New Tab", Content = "Content of new Tab" });
+        }
+
+        private void RemoveTab()
+        {
+            if (Tabs.Count > 0)
+            {
+                Tabs.RemoveAt(Tabs.Count - 1);
+            }
+        }
+    }
+
+    public class TabItemModel
+    {
+        public string Header { get; set; }
+        public string Content { get; set; }
+    }
+}
